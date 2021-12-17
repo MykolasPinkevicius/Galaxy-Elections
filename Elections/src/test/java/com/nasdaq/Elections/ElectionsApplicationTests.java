6290143016e9b6f16a2a77f28cda2d00a9d6ae6f
mykolas.pinkevicius <mykolas.pinkevicius@visma.com>
2021-12-17 17:13:56 +0200

    Sesame Initialize

---
+++ Elections/src/test/java/com/nasdaq/Elections/ElectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasdaq.Elections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
